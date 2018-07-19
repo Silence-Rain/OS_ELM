4e51be2f6f5d05c6c95a63a796848f4e854c8366
Silence <daniel.s.mo503@gmail.com>
2018-07-19 17:48:55 +0800

    优化所有功能

--- OS_ELM.py
+++ OS_ELM.py
@@ -12,6 +12,9 @@ class OS_ELM(object):
 		self.norm = norm
 		self.clear()
 
+	# 清空模型
+	# 随机生成输入层权重矩阵Iw，偏置向量bias
+	# 清空输出层权重矩阵beta，在线学习的中间权重矩阵M
 	def clear(self):
 		self.Iw = np.mat(np.random.rand(self.num_hidden_neurons, self.num_input_neurons) * 2 - 1)
 		self.bias = np.mat(np.random.rand(1, self.num_hidden_neurons))
@@ -23,7 +26,8 @@ class OS_ELM(object):
 		return (x - min) / (max - min)
 
 	# 使用0-1归一化，处理除label以外所有列的数据
-	# 参数：初始训练数据（np.array），label列的下标（默认为-1，无下标）
+	# @param data {np.array} 初始训练数据
+	# @param label_index {int} label列的下标（默认为-1，无下标）
 	def normalize(self, data, label_index=-1):
 		for ind in range(1,len(data[0])):
 			if ind != label_index:
@@ -34,11 +38,11 @@ class OS_ELM(object):
 
 		return data
 
-	# 激活函数
-	# 参数：tData: 样本矩阵
-	#	Iw: 输入层权重
-	#	bias: 隐层单元偏置
-	# 返回值：隐层输出矩阵
+	# sigmoid激活函数
+	# @param tData {np.array} 样本矩阵
+	# @param Iw {np.array} 输入层权重矩阵
+	# @param bias {np.array} 偏置向量
+	# @return {np.array} 隐层输出矩阵
 	def sig(self, tData, Iw, bias):
 		#样本数*隐含神经元个数
 		v = tData * Iw.T
@@ -48,16 +52,17 @@ class OS_ELM(object):
 		return H
 
 	# 获取数据中label列的取值范围
-	# 参数：label列
-	# 返回值：（label取值区间长度，label最小值）
+	# @param label {list} label列
+	# @return {tuple} label取值区间长度，label最小值
 	def get_label_range(self, label):
 		bot = min(label)
 		top = max(label)
 		return top - bot + 1, bot
 
 	# 使用初始数据训练网络
-	# 参数：初始训练数据（np.array），label列的下标（默认为0）
-	# 返回值：训练后的网络
+	# @param data {np.array} 初始训练数据
+	# @param label_index {int} label列的下标（默认为0）
+	# @return {OS_ELM} 训练后的网络
 	def fit_init(self, data, label_index=0):
 		label = []
 		matrix = []
@@ -65,11 +70,7 @@ class OS_ELM(object):
 		if self.norm == "0-1":
 			data = self.normalize(data, label_index)
 		data_size = len(data)
-		# 划分训练集和测试集
-		train_set = data[:int(0.8 * data_size)]
-		test_set = data[int(0.8 * data_size):]
-		print(len(train_set), len(test_set))
-		for row in train_set:
+		for row in data:
 			# 记录样本label
 			temp = []
 			label.append(int(row[label_index]))
@@ -92,20 +93,15 @@ class OS_ELM(object):
 		self.M = (H0.T * H0).I
 		# 计算输出权重
 		self.beta = self.M * H0.T * T0
+		# 打印训练准确率
+		self.error_calc(data)
 
-		# 训练准确率不足80%或测试准确率不足70%，则重新训练
-		train_err = self.error_calc(train_set, text="%s训练" % self.id)
-		test_err = self.error_calc(test_set, text="%s测试" % self.id)
-		if train_err < 0.80 or test_err < 0.70:
-			self.clear()
-			return False
-		else:
-			return self
-		# return self
+		return self
 
 	# 使用在线数据更新网络
-	# 参数：在线训练数据（np.array），label列的下标（默认为0）
-	# 返回值：更新后的网络
+	# @param data {np.array} 在线训练数据
+	# @param label_index {int} label列的下标（默认为0）
+	# @return {OS_ELM} 更新后的网络
 	def fit_train(self, data, label_index=0):
 		# 归一化数据
 		if self.norm == "0-1":
@@ -132,8 +128,8 @@ class OS_ELM(object):
 		return self
 
 	# 使用现有模型对数据分类
-	# 参数：需要分类的数据（np.array）
-	# 返回值：预测的label行
+	# @param data {np.array} 需要分类的数据
+	# @return {list} 预测结果
 	def predict(self, data):
 		res = []
 		# 归一化数据
@@ -153,7 +149,10 @@ class OS_ELM(object):
 		return res
 
 	# 计算训练的误差值
-	# 参数：训练数据，label列的下标（默认为0），输出内容的附加文本（默认为模型id）
+	# @param data {np.array} 训练数据
+	# @param label_index {int} label列的下标（默认为0）
+	# @param text {string} 输出内容的附加文本（默认为模型id）
+	# @return {float} 训练准确率
 	def error_calc(self, data, label_index=0, text=""):
 		# 附加文本默认为模型id
 		if len(text) == 0:
@@ -175,15 +174,3 @@ class OS_ELM(object):
 			sum += 1
 		print("%s准确性为：%f" % (text, correct / sum))
 		return correct / sum
-
-
-if __name__ == '__main__':
-	raw=np.loadtxt(open("data/trainData1111.csv","r"),delimiter=",",skiprows=0)
-	elm = OS_ELM(hidden_neuron=60, input_neuron=4, id="local_detection", norm="no")
-	network = elm.fit_init(data=raw)
-	while not network:
-		np.random.shuffle(raw)
-		network = elm.fit_init(data=raw)
-
-	res = network.predict(data=test)
-	print(res)
--- demo.py
+++ demo.py
@@ -4,17 +4,19 @@ import numpy as np
 from OS_ELM import OS_ELM
 
 if __name__ == '__main__':
+	# 读取数据
 	raw = np.loadtxt(open("data/test.csv", "r"), delimiter=",", skiprows=1)
 	# 按照4:1的比例分配训练集和测试集
 	train = raw[:650]
 	test = raw[650:]
-	# 原始数据已经归一化过，不使用模型本身的归一化
+	# 设置模型参数：
+	# 隐层节点：100
+	# 输入节点：19（根据输入数据的维度确定）
+	# 模型id：test_model
+	# 不进行归一化处理（原始数据已经归一化）
 	elm = OS_ELM(hidden_neuron=100, input_neuron=19, id="test_model", norm="no")
+	# 训练模型
 	network = elm.fit_init(data=train)
-	# 重新训练模型直至准确率符合要求
-	while not network:
-		np.random.shuffle(train)
-		network = elm.fit_init(data=train)
-
+	# 输出模型预测结果
 	res = network.predict(data=test[:,1:])
 	print(res)
